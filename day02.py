e6fa579826cd9cc8e86f0cf72f51eed16fe1d069
poojithaborpatla <borpatlapoojitha@gmail.com>
2026-09-11 12:02:50 +0530

    Data types

--- day02.py
+++ day02.py
@@ -1,1 +1,42 @@
-print(2)
+# int 
+a = 1234  #without point
+print(type(a))
+# float 
+a = 2.24 #with point
+print(type(a))
+# complex
+a = 4+4j
+print(type(a))
+# bool
+a =True
+print(type(a))
+# none
+a =None
+print(type(a))
+
+
+#sequences
+
+#list
+a=[1,2,3]
+print(type(a))
+#tuple
+b=(1,2,3,)
+print(type(b))
+#set
+c={1,2,3}
+print(type(c))
+#dict
+d={'a':1,'b':2,'c':3}
+print(type(d))
+#frozenset
+f=frozenset({1,2,3})
+print(type(f))
+#string
+s='poojitha'
+s="srinivasgoud"
+s='''poo'''
+print(type(s))
+#range
+r= range(2,5,3)
+print(type(r))
