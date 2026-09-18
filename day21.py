2b123e29f65e0048c856a28e10b343588635a94c
poojithaborpatla <borpatlapoojitha@gmail.com>
2026-09-18 10:59:59 +0530

    emailautomation

--- day21.py
+++ day21.py
@@ -2,8 +2,8 @@ import smtplib
 from email.message import EmailMessage
 
 # simple message
-sender = 'rakkeshadithya00@gmail.com' 
-password = 'silglmwkmtqxybct'
+sender = 'borpatlapoojitha@gmail.com'
+password = 'zfwb mseq cbpp hdiz'
 receiver = 'wd.rkad@gmail.com' 
 message = 'Hi rakesh, I have sent this mail from python code' 
 with smtplib.SMTP('smtp.gmail.com', 587) as conn:
@@ -13,15 +13,17 @@ with smtplib.SMTP('smtp.gmail.com', 587) as conn:
 print('Message sent successfully')
 
 
+
 # message with Subject and attachments
-sender = 'rakkeshadithya00@gmail.com' 
-password = 'silglmwkmtqxybct'
+
+sender = 'borpatlapoojitha@gmail.com' 
+password = 'zfwb mseq cbpp hdiz'
 message = EmailMessage()
-message['From'] = 'rakkeshadithya00@gmail.com'
-message['To'] = 'wd.rkad@gmail.com'
+message['From'] = 'borpatlapoojitha@gmail.com'
+message['To'] = 'skbashira2003@gmail.com'
 message['Subject'] = 'SMTP MAIL'
-message.set_content('Hi Rakesh, Iam sending this mail from Python Code')
-files = ['file_handling.png', 'php_record.pdf']
+message.set_content('Hi narmaja akka, Iam sending this mail from Python Code')
+files = ['Screenshot (153).png']
 for filename in files:
     with open(filename, 'rb') as f:
         file_data = f.read() 
@@ -31,26 +33,25 @@ with smtplib.SMTP('smtp.gmail.com', 587) as conn:
     conn.login(sender, password) 
     conn.send_message(message)
 print('Message sent successfully')
-#
 
 
 
 # Bulk mail sending with Subject and attachments
-sender = 'rakkeshadithya00@gmail.com' 
-password = 'silglmwkmtqxybct'
+sender = 'borpatlapoojitha@gmail.com' 
+password = 'zfwb mseq cbpp hdiz'
 receivers = ['wd.rkad@gmail.com', 'rakesh@codegnan.com']
 message = EmailMessage()
-message['From'] = 'rakkeshadithya00@gmail.com'
+message['From'] = 'borpatlapoojitha@gmail.com'
 message['Bcc'] = ','.join(receivers)
 message['Subject'] = 'SMTP MAIL'
-message.set_content('Hi Rakesh, Iam sending this mail from Python Code')
-files = ['file_handling.png']
+message.set_content('Hi Rakesh sir, Iam sending this mail from Python Code')
+files = ['Screenshot (153).png']
 for filename in files:
     with open(filename, 'rb') as f:
         file_data = f.read() 
         message.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=filename)
-with smtplib.SMTP('smtp.gmail.com', 587) as conn:
-    conn.starttls()
-    conn.login(sender, password) 
-    conn.send_message(message)
+    with smtplib.SMTP('smtp.gmail.com', 587) as conn:
+     conn.starttls()
+     conn.login(sender, password) 
+     conn.send_message(message)
 print('Message sent successfully')
